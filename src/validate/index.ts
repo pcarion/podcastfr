61dad1ad674dc6c62d2d41759260f59049989bcb
Pierre Carion <pcarion@gmail.com>
2021-01-02 10:13:54 -0800

    first runnable

--- src/validate/index.ts
+++ src/validate/index.ts
@@ -1,11 +1,14 @@
+import fs from 'fs-extra';
 import validateYamlFile from './validateYamlFile';
 import validateFeedUrls from './validateFeedUrls';
 import getPodcastDescriptionsFiles from './getPodcastDescriptionsFiles';
 import getPodcastFeedUrl from './getPodcastFeedUrl';
 import extractPodcastInfoFromRss from './extractPodcastInfoFromRss';
+import { Podcast } from '../jtd/podcast';
 
-async function validate(podcastsDirectory: string): Promise<void> {
+async function validate(podcastsDirectory: string, resultFile: string): Promise<void> {
   const files = await getPodcastDescriptionsFiles(podcastsDirectory);
+  const descriptions: Podcast[] = [];
   for (const fileName of files) {
     const description = await validateYamlFile(fileName);
     if (description.ignore) {
@@ -20,12 +23,21 @@ async function validate(podcastsDirectory: string): Promise<void> {
     if (rssUrl) {
       const info = await extractPodcastInfoFromRss(rssUrl);
       console.log(info);
+      descriptions.push({
+        information: info,
+        feedUrls: feedUrls,
+      });
     }
   }
+  console.log(descriptions);
+  await fs.writeJSON(resultFile, descriptions, {
+    spaces: 2,
+  });
 }
 
 const podcastsDirectory = './podcasts';
-validate(podcastsDirectory)
+const resultFile = './content/podcasts.json';
+validate(podcastsDirectory, resultFile)
   .then(() => {
     console.log('File is valid');
   })
