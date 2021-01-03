017c4e68a47eebb44e81c9d97fd6a938807997ac
Pierre Carion <pcarion@gmail.com>
2021-01-03 13:46:15 -0800

    get diff from PR

--- src/pr/index.ts
+++ src/pr/index.ts
@@ -1,6 +1,6 @@
 import * as core from '@actions/core';
 import { context, getOctokit } from '@actions/github';
-// import parse from 'parse-diff';
+import parse from 'parse-diff';
 
 async function run() {
   try {
@@ -16,6 +16,14 @@ async function run() {
     console.log('@@@ context:', Object.keys(context));
 
     console.log('@@ context.payload.pull_request:', context?.payload?.pull_request);
+
+    const diff_url = context.payload.pull_request?.diff_url;
+    if (!diff_url) {
+      throw new Error(`missing diff_url`);
+    }
+    const result = await octokit.request(diff_url);
+    const files = parse(result.data);
+    console.log('files in PR:', files);
   } catch (error) {
     core.setFailed(error.message);
   }
