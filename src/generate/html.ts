2552c60dc964124826923a4bca5e0552f7228b0b
Pierre Carion <pcarion@gmail.com>
2021-01-03 11:12:46 -0800

    add favicon

--- src/generate/html.ts
+++ src/generate/html.ts
@@ -6,6 +6,7 @@ export function htmlStart(lines: string[], title: string): void {
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>${title}</title>
     <link href="/assets/css/styles.css" rel="stylesheet">
+    <link rel="icon" href="/favicon.png">
   </head>
   <body class="bg-blue-200 font-sans">
     <div class="container mx-auto flex flex-col p-2 justify-center items-center max-w-full md:max-w-prose">
