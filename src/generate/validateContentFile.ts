0728bf42f9fa2fbdd2de38f7ffaee013e6ca62de
Pierre Carion <pcarion@gmail.com>
2021-01-03 16:27:08 -0800

    fix

--- src/generate/validateContentFile.ts
+++ src/generate/validateContentFile.ts
@@ -3,12 +3,12 @@ import { validate as validateAgainsJtdSchema } from 'jtd';
 import schema from '../jtd/podcast/schema';
 import { Podcast } from '../jtd/podcast';
 
-function validateJdtSchema(content: unknown): Podcast {
+function validateJdtSchema(content: unknown, fileName: string): Podcast {
   const validationErrors = validateAgainsJtdSchema(schema, content);
 
   if (validationErrors.length !== 0) {
     console.log(validationErrors);
-    throw new Error(`file is not valid (schema validation): ${validationErrors}`);
+    throw new Error(`file is not valid - ${fileName} - (schema validation): ${validationErrors}`);
   }
   return content as Podcast;
 }
@@ -22,5 +22,5 @@ export default async function validateContentFile(fileName: string): Promise<Pod
   if (!Array.isArray(doc)) {
     throw new Error(`content invalid (not an array)`);
   }
-  return doc.map((d) => validateJdtSchema(d));
+  return doc.map((d) => validateJdtSchema(d, fileName));
 }
