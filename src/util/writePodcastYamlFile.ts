8729c63faf648d087debb20651f8b745b5ab3c98
Pierre Carion <pcarion@gmail.com>
2021-01-08 07:56:57 -0800

    add contcts to yaml

--- src/util/writePodcastYamlFile.ts
+++ src/util/writePodcastYamlFile.ts
@@ -1,7 +1,7 @@
 import fs from 'fs-extra';
 import { Podcast } from '../jtd/podcast';
 
-function addProp(lines: string[], propName: string, value: string, indent = 0): void {
+function addProp(lines: string[], propName: string, value: string | undefined, indent = 0): void {
   let tab = '';
   for (let i = 0; i < indent; i++) {
     tab += '  ';
@@ -15,6 +15,14 @@ export default async function writePodcastYamlFile(podcast: Podcast, fileName: s
   addProp(lines, 'title', podcast.title);
   addProp(lines, 'description', podcast.description);
   addProp(lines, 'imageUrl', podcast.imageUrl);
+  lines.push('');
+  lines.push('contacts:');
+  addProp(lines, 'link', podcast.contacts?.link, 1);
+  addProp(lines, 'email', podcast.contacts?.email, 1);
+  addProp(lines, 'twitter', podcast.contacts?.twitter, 1);
+  addProp(lines, 'patreon', podcast.contacts?.patreon, 1);
+  addProp(lines, 'googleGroup', podcast.contacts?.googleGroup, 1);
+  lines.push('');
 
   await fs.writeFile(fileName, lines.join('\n'));
 }
