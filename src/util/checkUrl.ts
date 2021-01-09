cb8732e1390e58b0cd4dadcf61d0866c61dd0548
Pierre Carion <pcarion@gmail.com>
2021-01-09 14:20:57 -0800

    adding logs.again

--- src/util/checkUrl.ts
+++ src/util/checkUrl.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // follow URL redirect to get final landing URL
 export default async function checkUrl(url: string): Promise<string> {
-  console.log(`checking url: ${url}...`);
+  console.log(`checking url: ${url} type:${typeof url}...`);
   return new Promise((resolve, reject) => {
     axios
       .get(url)
