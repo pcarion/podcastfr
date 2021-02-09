92bff3e12883bad6099d26e4c686a94931afbd95
Pierre Carion <pcarion@gmail.com>
2021-02-09 08:18:59 -0800

    add tabs

--- netlify/www/src/components/single/appLink.tsx
+++ netlify/www/src/components/single/appLink.tsx
@@ -91,19 +91,131 @@ function renderLink(name: string, url: string, imagePath: string): ReactElement 
   );
 }
 
-const AppLink: FC<AppLinkProps> = ({ title, feeds }): ReactElement => {
+function renderIosLinks({ title, feeds }): ReactElement {
   const itunesId = extractItunesId(feeds.itunes);
 
   return (
+    <ul className="mt-3 grid grid-cols-1 gap-5 sm:gap-6">
+      {renderLink('Spotify', feeds.spotify, '/assets/logos/spotify.svg')}
+      {renderLink('Apple Podcasts', mkApplePodcastsUrl(title, itunesId), '/assets/logos/apple-podcast.svg')}
+      {renderLink('Google Podcasts', mkGooglePodcastsUrl(feeds.google), '/assets/logos/google-podcasts.svg')}
+      {renderLink('Overcast', mkOvercastUrl(feeds.rss), '/assets/logos/overcast.svg')}
+      {renderLink('Castro', mkCastroUrl(itunesId), '/assets/logos/castro.svg')}
+      {renderLink('Deezer', mkDeezerUrl(feeds.deezer), '/assets/logos/deezer.svg')}
+    </ul>
+  );
+}
+
+function renderAndroidLinks({ title, feeds }): ReactElement {
+  const itunesId = extractItunesId(feeds.itunes);
+
+  return (
+    <ul className="mt-3 grid grid-cols-1 gap-5 sm:gap-6">
+      {renderLink('Spotify', feeds.spotify, '/assets/logos/spotify.svg')}
+      {renderLink('Google Podcasts', mkGooglePodcastsUrl(feeds.google), '/assets/logos/google-podcasts.svg')}
+      {renderLink('Overcast', mkOvercastUrl(feeds.rss), '/assets/logos/overcast.svg')}
+      {renderLink('Castro', mkCastroUrl(itunesId), '/assets/logos/castro.svg')}
+      {renderLink('Deezer', mkDeezerUrl(feeds.deezer), '/assets/logos/deezer.svg')}
+    </ul>
+  );
+}
+
+function renderDesktopLinks({ title, feeds }): ReactElement {
+  const itunesId = extractItunesId(feeds.itunes);
+
+  return (
+    <ul className="mt-3 grid grid-cols-1 gap-5 sm:gap-6">
+      {renderLink('Spotify', feeds.spotify, '/assets/logos/spotify.svg')}
+      {renderLink('Apple Podcasts', mkApplePodcastsUrl(title, itunesId), '/assets/logos/apple-podcast.svg')}
+      {renderLink('Google Podcasts', mkGooglePodcastsUrl(feeds.google), '/assets/logos/google-podcasts.svg')}
+    </ul>
+  );
+}
+
+const Tabs = ({ color, title, feeds }) => {
+  const [openTab, setOpenTab] = React.useState(1);
+  return (
+    <>
+      <div className="flex flex-wrap">
+        <div className="w-full">
+          <ul className="flex mb-0 list-none flex-wrap pt-3 pb-4 flex-row" role="tablist">
+            <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
+              <a
+                className={
+                  'text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal ' +
+                  (openTab === 1 ? 'text-white bg-' + color + '-600' : 'text-' + color + '-600 bg-white')
+                }
+                onClick={(e) => {
+                  e.preventDefault();
+                  setOpenTab(1);
+                }}
+                data-toggle="tab"
+                href="#link1"
+                role="tablist"
+              >
+                iOS
+              </a>
+            </li>
+            <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
+              <a
+                className={
+                  'text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal ' +
+                  (openTab === 2 ? 'text-white bg-' + color + '-600' : 'text-' + color + '-600 bg-white')
+                }
+                onClick={(e) => {
+                  e.preventDefault();
+                  setOpenTab(2);
+                }}
+                data-toggle="tab"
+                href="#link2"
+                role="tablist"
+              >
+                Android
+              </a>
+            </li>
+            <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
+              <a
+                className={
+                  'text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal ' +
+                  (openTab === 3 ? 'text-white bg-' + color + '-600' : 'text-' + color + '-600 bg-white')
+                }
+                onClick={(e) => {
+                  e.preventDefault();
+                  setOpenTab(3);
+                }}
+                data-toggle="tab"
+                href="#link3"
+                role="tablist"
+              >
+                Desktop
+              </a>
+            </li>
+          </ul>
+          <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 ">
+            <div className="px-4 py-5 flex-auto">
+              <div className="tab-content tab-space">
+                <div className={openTab === 1 ? 'block' : 'hidden'} id="link1">
+                  {renderIosLinks({ title, feeds })}
+                </div>
+                <div className={openTab === 2 ? 'block' : 'hidden'} id="link2">
+                  {renderAndroidLinks({ title, feeds })}
+                </div>
+                <div className={openTab === 3 ? 'block' : 'hidden'} id="link3">
+                  {renderDesktopLinks({ title, feeds })}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+const AppLink: FC<AppLinkProps> = ({ title, feeds }): ReactElement => {
+  return (
     <div className="p-4">
-      <ul className="mt-3 grid grid-cols-1 gap-5 sm:gap-6">
-        {renderLink('Spotify', feeds.spotify, '/assets/logos/spotify.svg')}
-        {renderLink('Apple Podcasts', mkApplePodcastsUrl(title, itunesId), '/assets/logos/apple-podcast.svg')}
-        {renderLink('Google Podcasts', mkGooglePodcastsUrl(feeds.google), '/assets/logos/google-podcasts.svg')}
-        {renderLink('Overcast', mkOvercastUrl(feeds.rss), '/assets/logos/overcast.svg')}
-        {renderLink('Castro', mkCastroUrl(itunesId), '/assets/logos/castro.svg')}
-        {renderLink('Deezer', mkDeezerUrl(feeds.deezer), '/assets/logos/deezer.svg')}
-      </ul>
+      <Tabs color="blue" title={title} feeds={feeds} />
     </div>
   );
 };
